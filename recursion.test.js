4056dd7ff7dfea44770cfb23182223927ccc5f84
ali muhsen <ali-waleed96@hotmail.com>
2022-02-04 18:57:48 +0200

    solve recursion

--- recursion.test.js
+++ recursion.test.js
@@ -1,26 +1,30 @@
 /* Write a function to do the division operation without using the built-in division*/
 
-function division(number, dividedBy){
-    // Write you logic here.
-  return number/dividedBy;
-    
+function division(number, dividedBy) {
+    if (dividedBy == 0 || number == 0  ) {
+        return 0;
+    } else if (dividedBy == 1) {
+        return number;
+    } else {
+        return number / dividedBy;
+    }
 }
-
 /* Write a function that implement Math.pow(x,n) but using recursion
 Example:
 pow(2,4) = 16
 */
 
 
-function pow(x,n){
-if(n==0){
-    return 1;
-}else{
-    x= x*x;
-    n--;
-   return pow(x,n);
-}
-    
+function pow(x, n) {
+    if (n == 1) {
+        return x;
+    }
+    else if (n == 0) {
+        return 1;
+    } else {
+        return x * pow(x, n - 1);
+    }
+
 }
 
 /* The Fibonacci Series is a numeric series starting with the integers 0 and 1. In this series,
@@ -29,16 +33,16 @@ the next integer is determined by summing the previous two. This gives us:
 Write a function that take n as parameter and return the nth element in the Fibonacci Series
 Example: n = 4 ==> 3, n= 0 ==> 0, n = 3 ==> 2 */
 
-function fibonacci(n){
-    // Write you logic here.
-    if(n==0){
-return 0;
-    }else if(n==1){
-return 1;
-}else{
-    return fibonacci(n-1)+fibonacci(n-2);
+function fibonacci(n) {
 
-}
+    if (n == 0) {
+        return 0;
+    } else if (n == 1) {
+        return 1;
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+
+    }
 
 }
 
@@ -56,7 +60,7 @@ Example:
 Input: n = 3, k = 3
 Output: ["123", "132", "213", "231", "312", "321"] */
 
-function permutations(n,k){
+function permutations(n, k) {
     let arr = [];
     // Write you logic here. 
     return arr
@@ -65,18 +69,18 @@ function permutations(n,k){
 
 describe("Test division", () => {
     test("Return the division result", () => {
-        expect(division(10,2)).toStrictEqual(5);
-        expect(division(10,0)).toStrictEqual(0);
-        expect(division(0,10)).toStrictEqual(0);
-        
+        expect(division(10, 2)).toStrictEqual(5);
+        expect(division(10, 0)).toStrictEqual(0);
+        expect(division(0, 10)).toStrictEqual(0);
+
     })
 });
 
 describe("Test pow", () => {
     test("It should work as Math.pow(x,n)", () => {
-        expect(pow(10,2)).toStrictEqual(100);
-        expect(pow(10,0)).toStrictEqual(1);
-        expect(pow(0,0)).toStrictEqual(1);
+        expect(pow(10, 2)).toStrictEqual(100);
+        expect(pow(10, 0)).toStrictEqual(1);
+        expect(pow(0, 0)).toStrictEqual(1);
     })
 });
 
@@ -92,7 +96,7 @@ describe("Test fibonacci", () => {
 
 describe("Test permutations", () => {
     test("It should return a list of possible combinations", () => {
-        expect(permutations(3,3)).toStrictEqual(["123", "132", "213", "231", "312", "321"]);
-        expect(permutations(3,0)).toStrictEqual([]);
+        expect(permutations(3, 3)).toStrictEqual(["123", "132", "213", "231", "312", "321"]);
+        expect(permutations(3, 0)).toStrictEqual([]);
     })
 });
